7b236f2d83a834cfc807ef4cfe1f692dc06308f1
iyaz07 <moseseniade@gmail.com>
2023-12-17 10:08:46 -0800

    working on quick sort- some errors

--- sort.h
+++ sort.h
@@ -26,4 +26,8 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void swap(int *x, int *y);
+void quick_sort(int *array, size_t size);
+int partition(int *array, size_t low, size_t high, size_t size);
+int sort_q(int *array, size_t low, size_t high, size_t size);
 #endif
